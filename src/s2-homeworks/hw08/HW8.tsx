1fd4bd072f2ca473088cd2bd388ea0d6c7ba088f
DK-AC <Denkacaj@gmail.com>
2023-11-14 11:19:45 +0300

    hw 8 finished

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -85,8 +85,8 @@ const HW8 = () => {
                     <table id={'hw8-users'} className={s.users}>
                         <thead className={s.thead}>
                         <tr>
-                            <td className={s.nameCol}>Name</td>
-                            <td className={s.ageCol}>Age</td>
+                            <td className={s.tableHead}>Name</td>
+                            <td className={s.tableHead}>Age</td>
                         </tr>
                         </thead>
 
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,26 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+  switch (action.type) {
+    case 'sort': { // by name
+      const newState = [...state]
+      return newState.sort((a, b) => {
+        if (action.payload === 'up') {
+          return a.name.localeCompare(b.name)
+        } else {
+          return b.name.localeCompare(a.name)
         }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+      })
     }
+    case 'check': {
+
+      return state.filter(user => user.age > action.payload)
+    }
+    default:
+      return state
+  }
 }
