6fe2325c0ddfa14c2874fb62c0eea958ad0fd702
DK-AC <Denkacaj@gmail.com>
2023-11-14 14:10:03 +0300

    hw 12 finished

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,23 @@
 const initState = {
-    themeId: 1,
+  themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+type InitStateType = typeof initState
+type ActionsType = ReturnType<typeof changeThemeId>
 
-        default:
-            return state
+export const themeReducer = (state: InitStateType = initState, action: ActionsType): InitStateType => { // fix any
+  switch (action.type) {
+    // дописать
+    case 'SET_THEME_ID': {
+      return {
+        ...state,
+        themeId: action.id
+      }
     }
+
+    default:
+      return state
+  }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({type: 'SET_THEME_ID', id}) // fix any
